869c9c0d2f65fada4d4647ef29cb5ac345c41de8
hyvenet <hyvenet@haaxman.co.uk>
2024-11-27 11:10:31 +0000

    Stopped using fetch api for replacescript

--- replacescript.js
+++ replacescript.js
@@ -1,7 +1,15 @@
-fetch('hyvenet.github.io/page.html')
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(body) {
-    document.getElementsByTagName("html").innerHTML = body;
-  });
+function replacePage() {
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'https://hyvenet.github.io/page.html/', true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var resp = request.responseText;
+
+      document.querySelector('#html').innerHTML = resp;
+    }
+  };
+
+  request.send();
+}
